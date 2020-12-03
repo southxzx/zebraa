bc259e8ccf6f2152fb5b4d2eba7b68cc253f8e00
southxz <phuongnamsk109@gmail.com>
2020-12-03 17:43:58 +0700

    3/12

--- client/src/features/Home/Countdown/index.jsx
+++ client/src/features/Home/Countdown/index.jsx
@@ -1,61 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col,Button } from 'reactstrap';
+import Card from '../../../components/Card';
 import './countdown.css';
+
 function Countdown() {
+
+    const [isHovered1, setIsHovered1] = useState(false);
+    const [isHovered2, setIsHovered2] = useState(false);
+    const [isHovered3, setIsHovered3] = useState(false);
+
+    const handleHover1 = (e) => {
+        setIsHovered1(!isHovered1);
+        console.log(e);
+    }
+    const handleHover2 = () => {
+        setIsHovered2(!isHovered2);
+    }
+    const handleHover3 = () => {
+        setIsHovered3(!isHovered3);
+    }
+
     return (
         <section className="countdown">
-            <Container>
+            <Container fluid={true}>
                 <div className="countdown-title">
+                    <img src="Assets/images/hot-price.png" alt="deal"/>
                     <h2>Deal of the day</h2>
                 </div>
                 <div className="countdown-container">
                 <Row>
-                        <Col lg={4}>
-                            <div className="countdown-item">
-                                <div className="item-inner">
-                                    <div className="image">
-                                        <a>
-                                            <img src="Assets/images/product1.jpg"/>
-                                        </a>
-                                        <div className="time">
-                                            <div className="day">
-                                                <h2>07</h2>
-                                                <span>days</span>
-                                            </div>
-                                            <div className="hour">
-                                                <h2>12</h2>
-                                                <span>hours</span>
-                                            </div>
-                                            <div className="min">
-                                                <h2>45</h2>
-                                                <span>mins</span>
-                                            </div>
-                                            <div className="second">
-                                                <h2>12</h2>
-                                                <span>secs</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="caption">
-                                        <div className="rating-box">
-                                            <img src="Assets/images/rating.png"/>
-                                        </div>
-                                        <a className="product-name" href="#">Nike Air 4</a>
-                                        <div className="price-box">
-                                            <p className="new">$75.00</p>
-                                            <p className="old">$45.00</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </Col>
-                        <Col lg={4}>
+                    <Col>
+                        <div className="countdown-item" onMouseEnter={handleHover1} onMouseLeave={handleHover1}>
+                            <Card
+                                hover={isHovered1 ? "true" : null}
+                                active={isHovered1 ? "true" : null}
+                            />
+                        </div>
+                    </Col>                   
+                    <Col>
+                        <div className="countdown-item" onMouseEnter={handleHover3} onMouseLeave={handleHover3}>
+                            <Card
+                                active="true"
+                                hover={isHovered3 ? "true" : null}
+                            />
+                        </div>
+                    </Col>
 
-                        </Col>
-                        <Col lg={4}>
-
-                        </Col>
-                    
+                    <Col>
+                        <div className="countdown-item" onMouseEnter={handleHover2} onMouseLeave={handleHover2}>
+                            <Card
+                                hover={isHovered2 ? "true" : null}
+                                active={isHovered2 ? "true" : null}
+                            />
+                        </div>
+                    </Col>
                 </Row>
                 </div>
             </Container>
