e801bc2f4edff24d3d3e2c6437fc29dc0dba8d93
kienz7777 <kienvu7777@gmail.com>
2020-12-10 10:16:47 +0700

    add-clear nav product

--- client/src/features/Product/components/Product/index.jsx
+++ client/src/features/Product/components/Product/index.jsx
@@ -1,12 +1,12 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import './product.css';
 import { Container, Row, Col } from 'reactstrap';
+import InputRange from 'react-input-range';
 
 function Product(props) {
 
     //Att
     const [attribute,setAttribute] = useState([]);
-    var att = [];
 
     //category
     const cate = [`Shoe (${0})`,`Sandal (${0})`,`Tut (${0})`];
@@ -17,29 +17,57 @@ function Product(props) {
     ];
     var color1 = [],color2 = [];
     function divideColor(){
-        for(let i = 0 ; i < 4 ; i++){
-            color1[i] = color[i];
+        if(color.length >= 4 ){
+            for(let i = 0 ; i < 4 ; i++){
+                color1[i] = color[i];
+            }
+            for(let j = 4 ; j < color.length ; j++){
+                color2[j] = color[j];
+            }
         }
-        for(let j = 5 ; j < color.length ; j++){
-            color2[j] = color[j];
+        else{
+            for(let i = 0 ; i < color.length ; i++){
+                color1[i] = color[i];
+            }
         }
+
     }
     divideColor();
 
     //Manufacturer
-    const manuf = [`Nike (${0})`,`Adidas (${0})`,`Jordan (${0})`,`Balenciaga (${0})`];
-
+    //`Nike (${0})`,`Adidas (${0})`,`Jordan (${0})`,`Balenciaga (${0})`
+    const manuf = [
+        `Nike (${0})`,`Adidas (${0})`,`Jordan (${0})`,`Balenciaga (${0})`
+    ];
 
     //Attribute
-    function addAtt(item){
-        setAttribute(oldArray => [...oldArray, item]);
+    function addAtt(item){       
+        // cate.splice(cate.indexOf(item),1);
+        var i = item.indexOf(' ');
+        if(i > 0){
+            if(attribute.indexOf(item.substr(0,i)) >= 0)
+            return;
+            setAttribute(oldArray => [...oldArray, item.substr(0,i)]);
+        }
+        else{
+            if(attribute.indexOf(item) >= 0)
+            return;
+            setAttribute(oldArray => [...oldArray, item]);
+        }
+        
     }
     function clearAtt(item){
-        attribute.filter(item);
+        var filtered = attribute.filter(function(value, index, arr){
+            return value != item;
+        });
+        setAttribute(filtered);
+    }
+    function clearAll(){
+        setAttribute([]);
     }
 
     console.log(attribute);
-    console.log(att);
+
 
     return (
         <Container>
@@ -64,7 +92,7 @@ function Product(props) {
                                                             {item}
                                                         </div>
         
-                                                        <div className="clear">
+                                                        <div onClick={() => clearAtt(item)} className="clear">
                                                             <img src="/Assets/images/cancel.png"></img>
                                                         </div>
                                                     </div>
@@ -73,7 +101,7 @@ function Product(props) {
                                             }
                                         
 
-                                        <div className="clear-all">
+                                        <div onClick={()=>clearAll()} className="clear-all">
                                             Clear all
                                         </div>
                                     </div>
@@ -136,14 +164,14 @@ function Product(props) {
                                         <div className="item-colors">
                                                 {
                                                     color1.map((item,key) => (
-                                                        <span className="color" color={item} key={key}></span>
+                                                        <span onClick={()=> addAtt(item)} className="color" color={item} key={key}></span>
                                                     ))
                                                 }
                                         </div>
                                         <div className="item-colors">
                                                 {
                                                     color2.map((item,key) => (
-                                                        <span className="color" color={item} key={key}></span>
+                                                        <span onClick={()=> addAtt(item)} className="color" color={item} key={key}></span>
                                                     ))
                                                 }
                                         </div>
@@ -156,7 +184,7 @@ function Product(props) {
                                         <div className="list-item">
                                             {
                                                 manuf.map((item,key)=>(
-                                                    <div className="filter" key={key}>
+                                                    <div onClick={()=> addAtt(item)} className="filter" key={key}>
                                                         <span className="item" href="https://www.youtube.com/">
                                                             {item}
                                                         </span>
