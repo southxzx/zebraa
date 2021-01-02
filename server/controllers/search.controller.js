885b03cc4039e3e9fc0c24aecb060261e0a677d2
kienz7777 <kienvu7777@gmail.com>
2021-01-02 16:00:00 +0700

    search regex api

---
+++ server/controllers/search.controller.js
@@ -0,0 +1,45 @@
+const Product = require("../models/product.model");
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "");
+}
+
+module.exports.searchProduct = (req, res) => {
+    const regex1 = new RegExp("." + escapeRegex(req.query.name), "i");
+    const regex2 = new RegExp("^" + escapeRegex(req.query.name), "i"); // i change upperCase or lowerCase
+    // console.log(regex1);
+    // console.log(regex2);
+    Product.find(
+        {
+            name : { $in: [ regex1,regex2 ] }
+        }
+    ).
+    populate('category').
+    populate('colorProducts').
+    populate({ 
+        path: 'colorProducts',
+        populate: {
+          path: 'color'
+        } 
+    }).
+    populate({ 
+        path: 'review',
+        populate: {
+            path: 'user',
+            select: {'name':1}
+          }
+    }).
+    populate({ 
+        path: 'colorProducts',
+        populate: {
+          path: 'sizeProducts',
+          populate:{
+              path: 'size'
+          }
+        } 
+    }).
+    exec((err, data)=>{
+        if (err) return res.send(err);
+        res.status(200).send({data});
+    });
+}
