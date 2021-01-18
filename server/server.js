d6c2da00a338b3f42492c4eaac1528345db34108
kienz7777 <kienvu7777@gmail.com>
2021-01-19 01:24:25 +0700

    make it better

--- server/server.js
+++ server/server.js
@@ -18,9 +18,10 @@ connectDB()
 
 // Config for only development
 if (process.env.NODE_ENV === 'development') {
-    app.use(cors({
-        origin: process.env.CLIENT_URL
-    }))
+    // app.use(cors({
+    //     origin: process.env.CLIENT_URL
+    // }))
+    app.use(cors())
 
     //Morgan give information about each request
     //Cors it's allow to deal with react for localhost:3000 without any problem
